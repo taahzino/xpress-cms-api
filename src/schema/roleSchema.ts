5e97bce782de3fc36d005317b50731efad93114e
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-09-29 20:01:14 +0600

    feat: added zod validation schema for roles

---
+++ src/schema/roleSchema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import _constants from "../config/_constants";
+
+const validCapabilities = _constants.capabilities.map((x) => x.name);
+
+export const roleSchema = z.object({
+  name: z.string({
+    required_error: "Name is required",
+    invalid_type_error: "Name must be a string",
+  }),
+  capabilities: z
+    .array(
+      z
+        .string({
+          required_error: "Capabilities are required",
+          invalid_type_error: "Capabilities must be an array of strings",
+        })
+        .refine((value) => validCapabilities.includes(value) === true, {
+          message: "Invalid capability",
+        })
+    )
+    .min(1, "At least one capability is required")
+    .default([]),
+});
