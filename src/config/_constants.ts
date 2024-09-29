2d14c663c9fef2c76b3c9808bb3ec569b7463644
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-09-29 20:00:24 +0600

    feat: added role and user schema

---
+++ src/config/_constants.ts
@@ -0,0 +1,22 @@
+const _constants = {
+  capabilities: [
+    {
+      name: "manage-everything",
+      description: "Can manage everything",
+    },
+    {
+      name: "manage-users",
+      description: "Can manage users",
+    },
+    {
+      name: "manage-roles",
+      description: "Can manage roles",
+    },
+    {
+      name: "manage-pages",
+      description: "Can manage pages",
+    },
+  ],
+};
+
+export default _constants;
