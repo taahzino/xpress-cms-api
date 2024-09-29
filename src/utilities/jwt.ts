089896e90b9484fbe658b7cd875b22fcbcaed217
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-09-29 22:32:12 +0600

    feat: added utility for generating jwt token

---
+++ src/utilities/jwt.ts
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.JWT_SECRET || "JWT_SECRET_KEY";
+
+export const generateToken = (userId: number) => {
+  return jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: "1h" });
+};
