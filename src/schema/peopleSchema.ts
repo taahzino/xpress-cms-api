a4acd44800c3753780f14f26937f64a473484ace
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-09-29 22:32:35 +0600

    feat: added zod schema for validating people login

--- src/schema/peopleSchema.ts
+++ src/schema/peopleSchema.ts
@@ -34,3 +34,18 @@ export const peopleSchema = z.object({
     })
     .min(8, "Password must be at least 8 characters"),
 });
+
+export const peopleAuthSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email("Invalid email format"),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a string",
+    })
+    .min(8, "Password must be at least 8 characters"),
+});
