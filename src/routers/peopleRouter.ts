7f20cf01150b96cc5bc85bfb534aeb7e315b4107
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-09-29 22:37:56 +0600

    feat: added login protection for creating people

--- src/routers/peopleRouter.ts
+++ src/routers/peopleRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import createPeople from "../controllers/people/createPeople";
+import { peopleAuth } from "../middleware/auth/peopleAuth";
 
 const peopleRouter = Router();
 
-peopleRouter.post("/", createPeople);
+peopleRouter.post("/", peopleAuth, createPeople);
 
 export default peopleRouter;
