011e8fd6ed691cbfb5e2aea281929b0717953114
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-10-02 23:22:17 +0600

    chore: added documentation for /auth/login/people route

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -3,6 +3,38 @@ import loginPeople from "../controllers/auth/loginPeople";
 
 const authRouter = Router();
 
+/**
+ * @swagger
+ *
+ * /auth/login/people:
+ *   post:
+ *     summary: Login user
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "example@example.com"
+ *               password:
+ *                 type: string
+ *                 example: "password123"
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       400:
+ *         description: Invalid credentials
+ *       401:
+ *        description: Invalid email or password
+ */
+
 authRouter.post("/login/people", loginPeople);
 
 export default authRouter;
