3fb23c72c805e203ba4012f4c71eca330ab1da0a
Tahsin Ahmed Tushar <taahzino@gmail.com>
2024-10-03 20:22:19 +0600

    chore: increased jwt token validity to 7 days

--- src/utilities/jwt.ts
+++ src/utilities/jwt.ts
@@ -3,5 +3,5 @@ import jwt from "jsonwebtoken";
 const SECRET_KEY = process.env.JWT_SECRET || "JWT_SECRET_KEY";
 
 export const generateToken = (userId: number) => {
-  return jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: "1h" });
+  return jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: "7d" });
 };
